e7edc52d3bf12c0b4e4e34f1ba958be9b3ed2946
webs <weberswords@gmail.com>
2022-06-07 13:08:02 -0700

    Extract edge graph creation, add line circle plot

--- visualisations.R
+++ visualisations.R
@@ -1,13 +1,25 @@
 library("openxlsx") 
 library("tidyverse")
-library("igraph") 
+library("igraph")
+library("ggraph") 
 
 file = readline(prompt="Enter the name of the file: ")
 data=read.xlsx(file, 1)
-create_dot_plot <- function(data) {
+create_edge_graph <- function(data) {
     data %>% select(author_id, parent_question_user_id) -> edges
-    edge_graph=graph_from_data_frame(edges, directed=TRUE)
+    return(edge_graph=graph_from_data_frame(edges, directed=TRUE))
+}
+
+create_dot_plot <- function(data) {
+    edge_graph = create_edge_graph(data)
+    print("Creating dot plot...")
     plot(edge_graph, vertex.size=10, vertex.label=NA)
 }
 
-create_dot_plot(data)
+create_line_circle <- function(data) {
+    edge_graph = create_edge_graph(data)
+    ggraph(edge_graph, layout = 'circle') + geom_edge_link() + geom_node_point()
+}
+
+# create_dot_plot(data)
+create_line_circle(data)
